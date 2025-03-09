b058ce703fac2ecb5aba67c5c91662e4e32adc42
Mahmoud Ahmed Metwally <mahmmetwally26@gmail.com>
2025-03-09 04:54:16 +0200

    fix: update CORS origin to include API path for production

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://shoping-zeta-gold.vercel.app"],
+    origin: ["http://localhost:5173", "https://shoping-zeta-gold.vercel.app/api"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
